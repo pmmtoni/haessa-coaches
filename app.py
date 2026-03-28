9e7ad0ca6ef2b97c5b098e4a43494b56794c0c38
Paul Mmtoni Barasa <pmmtoni@gmail.com>
2026-03-28 16:08:59 +0200

    New Requirements

--- app.py
+++ app.py
@@ -26,7 +26,12 @@ if not database_url:
 database_url = database_url.strip()
 
 if database_url.startswith("postgres://"):
-    database_url = database_url.replace("postgres://", "postgresql://", 1)
+    database_url = database_url.replace("postgres://", "postgresql+psycopg://", 1)
+elif database_url.startswith("postgresql://"):
+    database_url = database_url.replace("postgresql://", "postgresql+psycopg://", 1)
+
+
+
 
 app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
